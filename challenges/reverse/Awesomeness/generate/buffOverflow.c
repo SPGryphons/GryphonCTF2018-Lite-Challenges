1ca94bf076151d113b183d9ade3117f6af3912f5
Noans <jonjontan610@hotmail.com>
2018-03-09 00:39:15 +0800

    Ensured players cannot gain flag just by looking at binary, touched up

--- challenges/reverse/Awesomeness/generate/buffOverflow.c
+++ challenges/reverse/Awesomeness/generate/buffOverflow.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-void win();
+
+void print(int * text, int i) {
+	i = i / sizeof(int);
+	i--;
+	for (int j = 0; i >= 0; i--, j++) {
+		int cs = text[i];
+		cs -= j + 1;
+		printf("%c", (char)cs);
+	}
+}
 
 void check() {
 	char buff[20];
@@ -9,7 +18,8 @@ void check() {
 }
 
 void admin() {
-	printf("\nGCTF{Y0UR_4W3S0M3_BU7_N07_R34LLY}\n");
+	int text[] = {37, -105, 76, 101, 71, 105, 72, 107, 71, 113, 99, 111, 100, 62, 102, 107, 94, 93, 60, 93, 78, -127, 75, 88, 70, 73, 11};
+	print(text, sizeof(text));
 }
 
 int main() {

91b63ac02141008dfc7cc30fcb2d166138895a0e
CK <exetrtck@gmail.com>
2018-04-02 02:57:33 +0800

    Initial Commit for HelloWorld

---
+++ challenges/reverse/HelloWorld/generate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press any key to exit");
+            Clipboard.SetText("GCTF{a_l1ttl3_3x7r4_d1dn7_hur7_n0b0dy}");
+            Console.ReadKey();
+        }
+    }
+}

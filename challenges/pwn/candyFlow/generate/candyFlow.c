346ba1f4b209be8a4585a5d8741e49b6987dcd61
IncompetentDev <kanghweethern@gmail.com>
2018-04-02 22:56:11 -0700

    Modified candyflow

--- challenges/pwn/candyFlow/generate/candyFlow.c
+++ challenges/pwn/candyFlow/generate/candyFlow.c
@@ -5,28 +5,31 @@ int main()
 {
 	setvbuf(stdout, NULL, _IONBF, 0);
 	
-	printf("Candy + Candy?\n\n");
+	printf("Long Long uCandy(%%llu) + Long Long uCandy = How many Candies?(%%d)\nI want -1 candies!\n\n");
 	
 	printf("How many candies?\n");
-	int number1=0;
-	scanf("%d", &number1); //non-integer values not read to stdin, shouldn't be a problem?
+	unsigned long long int candy1;
+	scanf("%llu", &candy1);
 	
 	printf("\nHow many more candies?\n");
-	int number2=0;
-	scanf("%d", &number2);
+	unsigned long long int candy2;
+	scanf("%llu", &candy2);
 	
-	if (number1<0 || number2<0)
+	int totalCandy;
+	totalCandy = candy1+candy2;
+	
+	if (candy1<0 || candy2<0)
 	{
 		printf("\nI can't count these candies.\n");
 	}
-	else if (number1+number2<0)
+	else if (totalCandy==-1)
 	{
-		printf("\nCandy + Candy = %d. Candy forever!\n", number1+number2);
+		printf("\nCandy + Candy = %d. Candy forever!\n", totalCandy);
 		system("/bin/cat flag.txt");
 	}
 	else
 	{
-		printf("\nCandy + Candy = %d. Good things never last :(\n", number1+number2);
+		printf("\nCandy + Candy = %d. Good things never last :(\n", totalCandy);
 	}
 }
 

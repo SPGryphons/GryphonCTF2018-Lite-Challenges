31a7fa438e3dba9ac22e92ff6916defb1fd24b21
PotatoDrug <zhy.david@gmail.com>
2018-04-04 23:36:53 +0800

    Switched to used NTP server

--- challenges/reverse/Crystal Ball/distrib/crystallball.js
+++ challenges/reverse/Crystal Ball/distrib/crystallball.js
@@ -18,8 +18,8 @@ fs.readFile('banner.txt', {encoding: 'utf-8'}, (err,data) => {
 });
 
 server.on('connection', sock => {
-    function random(s) {
-        let x = Math.sin(s) * 10000;
+    function a(y) {
+        let x = Math.sin(y) * 10000;
         return Math.floor((x - Math.floor(x)) * 10000000000);
     }
     sock.setEncoding('utf8');
@@ -31,11 +31,11 @@ server.on('connection', sock => {
                 console.error(err);
                 return;
             }
-            let seed = JSON.stringify(date).match(/T\d\d:\d\d/)[0].substring(1).split(':').map(Number).reduce((a, b) => a + b, 0);
-            if (seed === 0) {seed = 1}
+            let b = JSON.stringify(date).match(/T\d\d:\d\d/)[0].substring(1).split(':').map(Number).reduce((a, b) => a + b, 0);
+            if (b === 0) {b = 1}
             var recieved = parseInt(data.toString('utf8').replace(/\r?\n$/, ''));
-            if (recieved === random(seed)) {
-                sock.write('GCTF{PreD1C71ng_7HE_fU7URE_W17h_My_7rU57Y_crY574l84ll}');
+            if (recieved === a(b)) {
+                sock.write('GCTF{flag}'); // readacted flag
                 sock.destroy();
             } else {
                 sock.write('Wrong!');

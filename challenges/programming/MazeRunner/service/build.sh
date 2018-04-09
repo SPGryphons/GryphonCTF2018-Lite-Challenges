fe5a6641f545508c5cc3fa391f811e92cb561331
Noans <jonjontan610@hotmail.com>
2018-04-09 06:15:15 -0700

    Updated with hostnames and ports

--- challenges/programming/MazeRunner/service/build.sh
+++ challenges/programming/MazeRunner/service/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker build -t prog-mazerunner .
-docker run -d --restart always --memory 128M -p 10000:50000 --name prog-mazerunner prog-mazerunner
+docker run -d --restart always --memory 128M -p 18174:50000 --name prog-mazerunner prog-mazerunner
 docker start prog-mazerunner

b1f89ecb570e0fc1254841161a02cea44427279f
yz <yongze.dism@gmail.com>
2018-04-09 16:34:13 +0800

    updated lookclosely service

--- challenges/sanity/Look Closely/service/build.sh
+++ challenges/sanity/Look Closely/service/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -t look_closely .
-docker run --restart always --memory 64M -d -p 1501:80 --name san-look_closely look_closely
+docker run --restart always --memory 64M -d -p 18112:80 --name sanity-look_closely look_closely
 docker start san-look_closely

adb578719941d2492756d025f7771d8b57ae948a
PotatoDrug <zhy.david@gmail.com>
2018-04-12 20:49:23 +0800

    Fixed restart loop

--- challenges/pwn/Magician/service/build.sh
+++ challenges/pwn/Magician/service/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build -t magician .
-docker run --restart always --memory 128M -d -p 18155:5000 --name pwn-magician magician
+docker run --restart always --memory 128M -dt -p 18155:5000 --name pwn-magician magician
 docker start pwn-magician

